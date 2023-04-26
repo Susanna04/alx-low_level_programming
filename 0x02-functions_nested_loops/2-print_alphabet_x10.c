d68f021af30f0fc52129bf5edabf429669873158
Susanna04 <iyanusuzzy@gmail.com>
2023-04-26 08:13:33 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - check the code.
+ */
+
+ void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+			_putchar('\n');
+		}
+	}
+}
