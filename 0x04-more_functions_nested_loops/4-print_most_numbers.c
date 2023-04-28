993f9fddcdc38b41434d911dc10fd57b740bc485
Susanna04 <iyanusuzzy@gmail.com>
2023-04-28 14:04:21 -0700

    print numbers 1 to 9 except the frist two even numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers- function to not print 2 and 4
+ *
+ * Return: 1 to 9 except 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (!(x == 2 || x == 4))
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
