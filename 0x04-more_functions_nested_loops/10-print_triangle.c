16e13ce65f8b9c51587de80b8cb65beb0d153757
Susanna04 <iyanusuzzy@gmail.com>
2023-04-28 16:36:27 -0700

    print traingle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,12 +12,14 @@ void print_triangle(int size)
 	int r, h, s;
 
 	if (size <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
 		for (r = 1; r <= size; r++)
 		{
-			for (s = size - r; s >= 1; s++)
+			for (s = size - r; s >= 1; s--)
 			{
 				_putchar(' ');
 			}
