6d60df8d5732f3f44fbce988ec73af4d45962c1d
Susanna04 <iyanusuzzy@gmail.com>
2023-05-04 15:28:17 -0700

    copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copy string
+ * @dest: Destination
+ * @src: source
+ *@n: 
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	
+}
