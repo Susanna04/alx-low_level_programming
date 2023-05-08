3f4a52a62ec934e032a46ba34d45409714f87730
Susanna04 <iyanusuzzy@gmail.com>
2023-05-08 16:37:47 -0700

    lentgh of a preix string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substance
+ * @s: string
+ * @accept: input
+ * Return: Always 0.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int  i, n, x, result;
+
+	x = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		result = 0;
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (accept[n] == s[i])
+			{
+				x++;
+				result = 1;
+			}
+		}
+	}
+
+}
